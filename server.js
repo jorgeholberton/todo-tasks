7c8af5beb84a4d5a9542e600214ccb0db148df01
jorgeholberton <ingesoft99@gmail.com>
2023-08-14 21:08:10 -0500

    variables de entorno, modelo api, crear tarea

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');/* libreria que usa rest - http */
+const api = require('./api');
 const port = process.env.PORT || 3000; /* puerto de red */
 const app = express(); /* instancia de express */
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
 
 app.listen(port, function () {
     console.log("Server is listening at port: " + port);
@@ -9,3 +13,5 @@ app.listen(port, function () {
 app.get('/', function (req, res) {
     res.send("Respuesta Servidor");
 });
+
+app.use('/api', api);
